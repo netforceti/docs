df0b4ecb767289d8e7e7657e9c6f58826c8d5756
NetForceTI <bruno@netforce.com.br>
2021-05-21 20:19:33 +0000

    atualizando DOCS API | 10 | nf_api_docs:abb210b9-3110-4ec7-86e2-ac998bcf2110

--- docs/dev/api/config.js
+++ docs/dev/api/config.js
@@ -7,5 +7,7 @@ module.exports = [
         "/dev/api/portal/main.md",
         "/dev/api/portal/portal.md",
         "/dev/api/portal/auth.md",                
+        
+        "/dev/api/portal/parque.md",                
     ]}
 ];
