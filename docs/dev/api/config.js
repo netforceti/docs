518fa432a003c6c4636bac22197bf4942e0b4b03
NetForceTI <bruno@netforce.com.br>
2021-05-21 11:50:52 +0000

    atualizando DOCS API | 9 | nf_api_docs:a2d68846-bd93-44f9-885c-e103d4cff7f3

--- docs/dev/api/config.js
+++ docs/dev/api/config.js
@@ -2,4 +2,10 @@ module.exports = [
     "/dev/api/auth.md",
     "/dev/api/eventos.md",
     "/dev/api/eventos_sendpulse.md",
+
+    { title: "Portal", children: [
+        "/dev/api/portal/main.md",
+        "/dev/api/portal/portal.md",
+        "/dev/api/portal/auth.md",                
+    ]}
 ];
